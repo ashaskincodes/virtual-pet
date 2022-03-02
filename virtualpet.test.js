3be42c24f164aabc06d587a617f9f2a8370918ce
Ash Askin <ashaskincodes@gmail.com>
2022-03-02 11:38:40 +0000

    adjusted for REPL

--- virtualpet.test.js
+++
@@ -1,7 +0,0 @@
-const Pet = require("../virtual-pet/virtualpet");
-
-describe("Pet", () => {
-    it("Returns an object", () => {
-        expect(new Pet('Chairms')).toBeInstanceOf(Object);
-    });
-});
